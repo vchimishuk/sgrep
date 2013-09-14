85570605d80dfcb6a4ee9139c83543207b60173d
Viacheslav Chimishuk <voice@root.ua>
2013-09-14 15:54:31 +0300

    More tests added.

--- src/test.sh
+++ src/test.sh
@@ -41,10 +41,12 @@ echo 'd'           | ./sgrep 'a|b|c' > /dev/null; not_assert
 echo ''            | ./sgrep '(a|b|c)' > /dev/null; not_assert
 echo 'ab'          | ./sgrep '(ab|cd)' > /dev/null; assert
 echo 'ad'          | ./sgrep '(ab|cd)' > /dev/null; not_assert
-# echo 'ac'          | ./sgrep '(a|b)|(c|d)' > /dev/null; assert
-# echo 'ad'          | ./sgrep '(a|b)|(c|d)' > /dev/null; assert
-# echo 'bc'          | ./sgrep '(a|b)|(c|d)' > /dev/null; assert
-# echo 'bd'          | ./sgrep '(a|b)|(c|d)' > /dev/null; assert
+echo 'a'           | ./sgrep '(a|b)|(c|d)' > /dev/null; assert
+echo 'b'           | ./sgrep '(a|b)|(c|d)' > /dev/null; assert
+echo 'c'           | ./sgrep '(a|b)|(c|d)' > /dev/null; assert
+echo 'd'           | ./sgrep '(a|b)|(c|d)' > /dev/null; assert
+echo 'ac'          | ./sgrep '(a|b)(c|d)' > /dev/null; assert
+echo 'bd'          | ./sgrep '(a|b)(c|d)' > /dev/null; assert
 
 echo 'aaacddd'     | ./sgrep 'a*(b|c)d+' > /dev/null; assert
 echo 'cd'          | ./sgrep 'a*(b|c)d+' > /dev/null; assert
